0f26c94fd684442aa480a367d0b137c76956b649
aldi-balkar <aldibalkar23@gmail.com>
2023-05-14 17:50:57 +0700

    CRUD API Categories

---
+++ app/api/v1/categories/controller.js
@@ -0,0 +1,82 @@
+const Categories = require('./model');
+
+const create = async (req, res, next) => {
+    try {
+        const { name } = req.body;
+        const result = await Categories.create({ name });
+        res.status(201).json({
+            data: result,
+        });
+    } catch (err) {
+        next(err);
+    }
+};
+
+const index = async (req, res, next) => {
+    try {
+        const result = await Categories.find(); // BISA DI SELECT BY DATA
+        res.status(200).json({
+            data: result,
+        });
+    } catch (err) {
+        next(err);
+    }
+};
+
+const find = async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const result = await Categories.findOne({ _id: id });
+
+        if (!result) {
+            return res.status(404).json({ message: 'Id Categories tidak di temukan' });
+        }
+
+        res.status(200).json({
+            data: result,
+        });
+    } catch (err) {
+        next(err);
+    }
+};
+
+    const update = async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const { name } = req.body;
+
+        // cari dan update categories berdasarkan field _id
+        const result = await Categories.findOneAndUpdate(
+        { _id: id },
+        { name },
+        { new: true, runValidators: true } // menampilkan data baru dan menjalankan validation
+        );
+
+        res.status(200).json({
+        data: result,
+        });
+    } catch (err) {
+        next(err);
+    }
+    };
+
+
+const destroy = async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const result = await Categories.findByIdAndRemove(id);
+        res.status(200).json({
+            data: result,
+        });
+    } catch (err) {
+        next(err);
+    }
+};
+
+module.exports = {
+    index,
+    create,
+    find,
+    update,
+    destroy,
+}
